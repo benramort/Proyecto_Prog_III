a82306b763323c071ce3a68c259e6b5404b3342d
Beñat Ramirez <benat.ramirez@opendeusto.es>
2023-10-11 18:05:49 +0200

    Inicio de sesión sin integrar

--- Proyecto_Prog_III/src/ventanas/IniciarSesion.java
+++ Proyecto_Prog_III/src/ventanas/IniciarSesion.java
@@ -1,8 +1,12 @@
 package ventanas;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -23,14 +27,14 @@ public class IniciarSesion extends JFrame {
 		setSize(500, 300);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE); //Por qué exit on close??
+//		getContentPane().setBackground(Color.WHITE);
 		
 		//Crear contenedores
 		JPanel pInferior = new JPanel();
 		JPanel pInferiorBox = new JPanel();
+		JPanel pUsuarioContrasena = new JPanel();
 		pInferiorBox.setLayout(new BoxLayout(pInferiorBox, BoxLayout.Y_AXIS));
-		JPanel pUsuario = new JPanel();
-		JPanel pContrasena = new JPanel();
-		JPanel pBotonera = new JPanel();
+		JPanel pBotonera = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 5));
 		
 		JPanel pTexto = new JPanel();
 		pTexto.setLayout(new BoxLayout(pTexto, BoxLayout.Y_AXIS));
@@ -38,22 +42,28 @@ public class IniciarSesion extends JFrame {
 		pCampos.setLayout(new BoxLayout(pCampos, BoxLayout.Y_AXIS));
 		
 		//Formato de contenedores
-		
+//		pInferior.setOpaque(false);
 		
 		//Crear componentes
 		JLabel lUsuario = new JLabel("Usuario:");
 		JTextField tfUsuario = new JTextField(15);
 		JLabel lContrasena = new JLabel("Contraseña:");
 		JPasswordField pfContrasena = new JPasswordField(15);
+		JButton btIniciarSesion = new JButton("Iniciar sesión");
+		JButton btNuevaCuenta = new JButton("Crear cuenta");
 		
 		//Formato componentes
 		Font fuente = new Font("Arial", Font.BOLD, 15);
 		lUsuario.setFont(fuente);
 		lContrasena.setFont(fuente);
 		
+		
 		//Añadir componentes a contenedores
 		add(pInferior, BorderLayout.SOUTH);
-//		pInferior.add(pInferiorBox);
+		pInferior.add(pInferiorBox);
+		pInferiorBox.add(pUsuarioContrasena);
+		pInferiorBox.add(pBotonera);
+		
 //		pInferiorBox.add(pUsuario);
 //		pInferiorBox.add(pContrasena);
 //		pUsuario.add(lUsuario);
@@ -61,16 +71,41 @@ public class IniciarSesion extends JFrame {
 //		pContrasena.add(lContrasena);
 //		pContrasena.add(pfContrasena);
 		
-		pInferior.add(pTexto);
+		pUsuarioContrasena.add(pTexto);
 		pTexto.add(lUsuario);
-		pTexto.add(Box.createVerticalStrut(5));
+		pTexto.add(Box.createVerticalStrut(5)); //Añade distancia vertical
 		pTexto.add(lContrasena);
-		pInferior.add(pCampos);
+		pUsuarioContrasena.add(pCampos);
 		pCampos.add(tfUsuario);
 		pCampos.add(Box.createVerticalStrut(5));
 		pCampos.add(pfContrasena);
+		pBotonera.add(btIniciarSesion);
+		pBotonera.add(btNuevaCuenta);
+		
+		
+		//Configurar escuchadores
+		btNuevaCuenta.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				SwingUtilities.invokeLater(new Runnable() {
+					@Override
+					public void run() {
+						new CrearCuenta();
+					}
+				});
+				dispose();
+			}
+		});
+		
+		
+		
+		
+		
 		
 		setVisible(true);
+		
+		
 		
 		
 	}
