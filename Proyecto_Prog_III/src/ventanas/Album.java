6db0dc45cabc21e0f8bade08d49a70d4b49b8ae7
Iker <Iker@DESKTOP-RIC0F0U>
2023-10-12 19:01:23 +0200

    Añadidas imagenes a los botones. (Imagenes provisionales)

--- Proyecto_Prog_III/src/ventanas/Album.java
+++ Proyecto_Prog_III/src/ventanas/Album.java
@@ -1,10 +1,13 @@
 package ventanas;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Image;
 
 import javax.swing.*;
+import javax.swing.border.*;
 
 
 public class Album extends JFrame {
@@ -29,20 +32,49 @@ public class Album extends JFrame {
 		//Formato contenedores
 		pIzquierdo.setLayout(new BorderLayout());
 		pBotones.setLayout(new GridLayout(2, 3));
+		
+		Border bordePanelIzquierdo = BorderFactory.createLineBorder(Color.BLACK);
+		pIzquierdo.setBorder(bordePanelIzquierdo);
 		//Crear componentes
-		JButton bIdle = new JButton("IDLE");
-		JButton bMercado = new JButton("MERCADO");
-		JButton bTienda = new JButton("TIENDA");
-		JButton bAjustes = new JButton("AJUSTES");
-		JButton bSalir = new JButton("SALIR");
-		JButton bFiltros = new JButton("FILTROS");
+		JButton bIdle = new JButton();
+		JButton bMercado = new JButton();
+		JButton bTienda = new JButton();
+		JButton bAjustes = new JButton();
+		JButton bSalir = new JButton();
+		JButton bBuscar = new JButton();
+		
+		ImageIcon imagen = new ImageIcon(getClass().getResource("/idle.png"));
+		ImageIcon imagenIdle = new ImageIcon(imagen.getImage().getScaledInstance(70, 70, Image.SCALE_DEFAULT));
+		
+		ImageIcon imagen2 = new ImageIcon(getClass().getResource("/ajustes.png"));
+		ImageIcon imagenAjustes = new ImageIcon(imagen2.getImage().getScaledInstance(70, 70, Image.SCALE_DEFAULT));
+		
+		ImageIcon imagen3 = new ImageIcon(getClass().getResource("/lupa.png"));
+		ImageIcon imagenLupa = new ImageIcon(imagen3.getImage().getScaledInstance(70, 70, Image.SCALE_DEFAULT));
+		
+		ImageIcon imagen4 = new ImageIcon(getClass().getResource("/mercado.png"));
+		ImageIcon imagenMercado = new ImageIcon(imagen4.getImage().getScaledInstance(70, 70, Image.SCALE_DEFAULT));
+		
+		ImageIcon imagen5 = new ImageIcon(getClass().getResource("/tienda.png"));
+		ImageIcon imagenTienda = new ImageIcon(imagen5.getImage().getScaledInstance(70, 70, Image.SCALE_DEFAULT));
+		
+		ImageIcon imagen6 = new ImageIcon(getClass().getResource("/salir.png"));
+		ImageIcon imagenSalir = new ImageIcon(imagen6.getImage().getScaledInstance(70, 70, Image.SCALE_DEFAULT));
+		
+		
 		//Formato componentes
+		bIdle.setIcon(imagenIdle);
+		bAjustes.setIcon(imagenAjustes);
+		bBuscar.setIcon(imagenLupa);
+		bMercado.setIcon(imagenMercado);
+		bTienda.setIcon(imagenTienda);
+		bSalir.setIcon(imagenSalir);
 		bIdle.setPreferredSize(new Dimension(150,  100));
 		bMercado.setPreferredSize(new Dimension(150,  100));
 		bTienda.setPreferredSize(new Dimension(150,  100));
 		bAjustes.setPreferredSize(new Dimension(150,  100));
 		bSalir.setPreferredSize(new Dimension(150,  100));
-		bFiltros.setPreferredSize(new Dimension(150,  100));
+		bBuscar.setPreferredSize(new Dimension(150,  100));
 		//Añadir componentes a contenedores
 		this.getContentPane().add(pIzquierdo, BorderLayout.WEST);
 		this.getContentPane().add(pCartas, BorderLayout.CENTER);
@@ -50,9 +82,9 @@ public class Album extends JFrame {
 		pBotones.add(bIdle);
 		pBotones.add(bMercado);
 		pBotones.add(bTienda);
+		pBotones.add(bBuscar);
 		pBotones.add(bAjustes);
 		pBotones.add(bSalir);
-		pBotones.add(bFiltros);
 		
 		setVisible(true);
 	}
