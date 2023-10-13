171b3aaf7bbb3ba1083a8f2ba90319a0077f5902
GaizkaCarrasco <gaizka.carrasco@opendeusto.es>
2023-10-13 18:30:10 +0200

    Ventana Tienda(Para una carta)

--- Proyecto_Prog_III/src/ventanas/Tienda.java
+++ Proyecto_Prog_III/src/ventanas/Tienda.java
@@ -20,17 +20,27 @@ public class Tienda extends JFrame{
 		JPanel pMonedas = new JPanel();
 		JPanel pCentro = new JPanel();
 		JScrollPane sc = new JScrollPane(pCentro);
+		JPanel pSobre = new JPanel();
+		JPanel pNombreSobre = new JPanel();
+		JPanel pPrecioSobre = new JPanel();
 		
 		///Formato Contenedores
 		pSuperior.setLayout(new BorderLayout());
 		pMonedas.setLayout(new FlowLayout((FlowLayout.RIGHT)));
 		sc.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		sc.setBorder(null);
+		pSobre.setLayout(new BorderLayout());
+		pNombreSobre.setLayout(new FlowLayout(FlowLayout.CENTER));
+		pPrecioSobre.setLayout(new FlowLayout(FlowLayout.CENTER));
 		
 		///Crear Componentes
 		JButton btHome = new JButton("HOME");
 		JLabel lMonedas = new JLabel("XXXXXXXXXX");
 		JLabel lImagenMonedas = new JLabel();
+		JLabel lNombreSobre = new JLabel("Megasobre");
+		JLabel lPrecioSobre = new JLabel("50.000");
+		JLabel lImagenSobre = new JLabel(new ImageIcon("img/yoshi.png"));
+		
 		
 		ImageIcon imagen7 = new ImageIcon(getClass().getResource("/moneda.png"));
 		ImageIcon imagenMoneda = new ImageIcon(imagen7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT));
@@ -43,6 +53,14 @@ public class Tienda extends JFrame{
 		add(pSuperior, BorderLayout.NORTH);
 		pMonedas.add(lMonedas);
 		pMonedas.add(lImagenMonedas);
+		pNombreSobre.add(lNombreSobre);
+		pPrecioSobre.add(lPrecioSobre);
+		pPrecioSobre.add(lImagenMonedas);
+		pSobre.add(pNombreSobre,BorderLayout.NORTH);
+		pSobre.add(lImagenSobre, BorderLayout.CENTER);
+		pSobre.add(pPrecioSobre,BorderLayout.SOUTH);
+		pCentro.add(pSobre, BorderLayout.CENTER);
+		
 		
 		getContentPane().add(sc, BorderLayout.CENTER );
 		
