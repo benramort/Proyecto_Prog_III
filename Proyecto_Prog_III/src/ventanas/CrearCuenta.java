f9fd955041c2a792ac423851d5d644884f70bd86
Iker <Iker@DESKTOP-RIC0F0U>
2023-10-14 19:43:04 +0200

    Cambiado el color del panel en CrearCuenta

--- Proyecto_Prog_III/src/ventanas/CrearCuenta.java
+++ Proyecto_Prog_III/src/ventanas/CrearCuenta.java
@@ -10,7 +10,7 @@ public class CrearCuenta extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private static final Color c = new Color(84,241,255);
+	private static final Color c = new Color(42,215,245);
 
 
 	
