3bdd7370faeea0cc078cb44d0ae2715e5ca05a23
Iker <Iker@DESKTOP-RIC0F0U>
2023-10-19 14:57:43 +0200

    Añadidos paneles de monedas y barra de progreso a ventana entrenamiento

--- Proyecto_Prog_III/src/ventanas/Entrenamiento.java
+++ Proyecto_Prog_III/src/ventanas/Entrenamiento.java
@@ -1,6 +1,9 @@
 package ventanas;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Image;
 
 import javax.swing.*;
 
@@ -19,19 +22,46 @@ public class Entrenamiento extends JFrame{
 		this.getContentPane().setLayout(new BorderLayout());
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		//Creacion contenedores
-		JPanel pSuperior = new JPanel();
 		JPanel pInferior = new JPanel();
+		JPanel pCentral = new JPanel();
+		JPanel pBotonClear = new JPanel();
+		JPanel pTextos = new JPanel();
+		JPanel pBarraProgreso = new JPanel();
 		//Formato contenedores
-		
+		pInferior.setLayout(new BorderLayout());
+		pTextos.setLayout(new FlowLayout(FlowLayout.CENTER));
+		pCentral.setLayout(new BorderLayout());
 		//Creacion componentes
 		JButton bAlbum = new JButton("ÁLBUM");
 		JLabel lMonedasGeneradas = new JLabel("Monedas generadas: " );
+		JLabel lMonedasGeneradas2 = new JLabel("XXXXXX");
 		JLabel lMonedasPorMinuto = new JLabel("Monedas/minuto: " );
+		JLabel lMonedasPorMinuto2 = new JLabel("XXXXXX" );
+		JLabel lImagenMonedasGeneradas = new JLabel();
+		JLabel lImagenMonedasPorMinuto = new JLabel();
 		JButton bClear = new JButton("CLEAR");
 		JProgressBar pbProgreso = new JProgressBar(0, 100);
+		ImageIcon imagen = new ImageIcon(getClass().getResource("/moneda.png"));
+		ImageIcon imagenMoneda = new ImageIcon(imagen.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT));
 		//Formato componentes
-		
+		pbProgreso.setPreferredSize(new Dimension(1000, 30));
 		//Añadir componentes a contenedores
+		this.getContentPane().add(pInferior, BorderLayout.SOUTH);
+		this.getContentPane().add(pCentral, BorderLayout.CENTER);
+		pInferior.add(pBotonClear, BorderLayout.EAST);
+		pBotonClear.add(bClear);
+		pInferior.add(pTextos, BorderLayout.CENTER);
+		pTextos.add(lMonedasGeneradas);
+		pTextos.add(lMonedasGeneradas2);
+		pTextos.add(lImagenMonedasGeneradas);
+		lImagenMonedasGeneradas.setIcon(imagenMoneda);
+		pTextos.add(Box.createHorizontalStrut(200));
+		pTextos.add(lMonedasPorMinuto);
+		pTextos.add(lMonedasPorMinuto2);
+		pTextos.add(lImagenMonedasPorMinuto);
+		lImagenMonedasPorMinuto.setIcon(imagenMoneda);
+		pBarraProgreso.add(pbProgreso);
+		pCentral.add(pBarraProgreso, BorderLayout.SOUTH);
 		
 		setVisible(true);
 		
