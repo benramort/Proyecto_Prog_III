bfa090b3807e0501e41e12f930aeb5e499f55664
GaizkaCarrasco <gaizka.carrasco@opendeusto.es>
2023-10-21 16:26:50 +0200

    Tienda Actualizada

--- Proyecto_Prog_III/src/ventanas/Tienda.java
+++ Proyecto_Prog_III/src/ventanas/Tienda.java
@@ -1,5 +1,4 @@
 package ventanas;
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -8,101 +7,121 @@ import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.*;
+public class Tienda extends JFrame {
 
-public class Tienda extends JFrame{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 
-	public Tienda(JFrame ventanaAnterior) {
-		///Formato Ventana
-		setSize(1500,1000);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setTitle("Universal Card Collection");
-		setLocationRelativeTo(ventanaAnterior);
-		
-		///Crear Contenedores
-		JPanel pSuperior = new JPanel();
-		JPanel pMonedas = new JPanel();
-		JPanel pCentro = new JPanel();
-		JScrollPane sc = new JScrollPane(pCentro);
-		JPanel pSobre = new JPanel();
-		JPanel pNombreSobre = new JPanel();
-		JPanel pPrecioSobre = new JPanel();
-		JPanel pBotonHome = new JPanel();
-		
-		///Formato Contenedores
-		pSuperior.setLayout(new BorderLayout());
-		pMonedas.setLayout(new FlowLayout((FlowLayout.RIGHT)));
-		sc.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		sc.setBorder(null);
-		pSobre.setLayout(new BorderLayout());
-		pNombreSobre.setLayout(new FlowLayout(FlowLayout.CENTER));
-		pPrecioSobre.setLayout(new FlowLayout(FlowLayout.CENTER));
-		
-		///Crear Componentes
-		JButton btHome = new JButton("ÁLBUM");
-		JLabel lMonedas = new JLabel("XXXXXXXXXX");
-		JLabel lImagenMonedas = new JLabel();
-		JLabel lImagenMonedas2 = new JLabel();
-		JLabel lNombreSobre = new JLabel("Megasobre");
-		JLabel lPrecioSobre = new JLabel("50.000");
-		JLabel lImagenSobre = new JLabel(new ImageIcon("img/yoshi.png"));
-		
-		
-		ImageIcon imagen7 = new ImageIcon(getClass().getResource("/moneda.png"));
-		ImageIcon imagenMoneda = new ImageIcon(imagen7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT));
-		ImageIcon imagenMoneda2 = new ImageIcon(imagen7.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
-		///Formato componentes
-		lImagenMonedas.setIcon(imagenMoneda);
-		lImagenMonedas2.setIcon(imagenMoneda2);
-		Font fuenteNombre = new Font("Arial",Font.BOLD, 32);
-		lNombreSobre.setFont(fuenteNombre);
-		lPrecioSobre.setFont(fuenteNombre);
-		btHome.setPreferredSize(new Dimension(90, 40));
-		
-		///Añadir componentes a contenedores
-		pSuperior.add(pBotonHome, BorderLayout.WEST);
-		pBotonHome.add(btHome);
-		pSuperior.add(pMonedas, BorderLayout.EAST);
-		add(pSuperior, BorderLayout.NORTH);
-		pMonedas.add(lMonedas);
-		pMonedas.add(lImagenMonedas);
-		pNombreSobre.add(lNombreSobre);
-		pPrecioSobre.add(lPrecioSobre);
-		pPrecioSobre.add(lImagenMonedas2);
-		pSobre.add(pNombreSobre,BorderLayout.NORTH);
-		pSobre.add(lImagenSobre, BorderLayout.CENTER);
-		pSobre.add(pPrecioSobre,BorderLayout.SOUTH);
-		pCentro.add(pSobre, BorderLayout.CENTER);
-		
-		
-		getContentPane().add(sc, BorderLayout.CENTER );
-		
-		
-		setVisible(true);
-		
-		btHome.addActionListener(new ActionListener() {
+		public Tienda (JFrame ventanaAnterior) {
+			List<String> nombres = new ArrayList<String>();
+			nombres.add("Megasobre");
+			nombres.add("Sobre Ultimate");
+			nombres.add("Sobre Oro");
+			nombres.add("Sobre Oro Premium");
 			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-				
+			List<String> precios = new ArrayList<String>();
+			precios.add("50000");
+			precios.add("125000");
+			precios.add("7500");
+			precios.add("15000");
+			///Formato Ventana
+			setSize(1500,1000);
+			setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+			setTitle("Universal Card Collection");
+			setLocationRelativeTo(ventanaAnterior);
+			
+			///Crear Contenedores
+			JPanel pSuperior = new JPanel();
+			JPanel pMonedas = new JPanel();
+			JPanel pCentro = new JPanel();
+			JScrollPane sc = new JScrollPane(pCentro);
+			JPanel pBotonHome = new JPanel();
+			
+			///Formato Contenedores
+			pSuperior.setLayout(new BorderLayout());
+			pMonedas.setLayout(new FlowLayout((FlowLayout.RIGHT)));
+			sc.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+			sc.setBorder(null);
+			
+			///Crear Componentes
+			JButton btHome = new JButton("ÁLBUM");
+			JLabel lMonedas = new JLabel("XXXXXXXXXX");
+			JLabel lImagenMonedas = new JLabel();
+			
+			ImageIcon imagen7 = new ImageIcon(getClass().getResource("/moneda.png"));
+			ImageIcon imagenMoneda = new ImageIcon(imagen7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT));
+			
+			///Formato componentes
+			lImagenMonedas.setIcon(imagenMoneda);
+			Font fuenteNombre = new Font("Arial",Font.BOLD, 32);
+			btHome.setPreferredSize(new Dimension(90, 40));
+			
+			///Añadir componentes a contenedores
+			pSuperior.add(pBotonHome, BorderLayout.WEST);
+			pBotonHome.add(btHome);
+			pSuperior.add(pMonedas, BorderLayout.EAST);
+			add(pSuperior, BorderLayout.NORTH);
+			pMonedas.add(lMonedas);
+			pMonedas.add(lImagenMonedas);
+			
+			
+			for (int i = 0; i < nombres.size(); i++) {
+				//Crear Contenedores
+				JPanel pSobre = new JPanel();
+				JPanel pNombreSobre = new JPanel();
+				JPanel pPrecioSobre = new JPanel();
+				JLabel lImagenMonedas2 = new JLabel();
+				//Formato Contenedores
+				pSobre.setLayout(new BorderLayout());
+				pNombreSobre.setLayout(new FlowLayout(FlowLayout.CENTER));
+				pPrecioSobre.setLayout(new FlowLayout(FlowLayout.CENTER));
+				//Crear Componentes
+				JLabel lNombreSobre = new JLabel(nombres.get(i));
+				JLabel lPrecioSobre = new JLabel(precios.get(i));
+				JLabel lImagenSobre = new JLabel(new ImageIcon("img/yoshi.png"));
+				ImageIcon imagenMoneda2 = new ImageIcon(imagen7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT));
+				//Formato Componentes
+				lImagenMonedas2.setIcon(imagenMoneda2);
+				lNombreSobre.setFont(fuenteNombre);
+				lPrecioSobre.setFont(fuenteNombre);
+				//Añadir componentes a contenedores
+				pNombreSobre.add(lNombreSobre);
+				pPrecioSobre.add(lPrecioSobre);
+				pPrecioSobre.add(lImagenMonedas2);
+				pSobre.add(pNombreSobre,BorderLayout.NORTH);
+				pSobre.add(lImagenSobre, BorderLayout.CENTER);
+				pSobre.add(pPrecioSobre,BorderLayout.SOUTH);
+				pCentro.add(pSobre, BorderLayout.CENTER);
 			}
-		});
+			
+			getContentPane().add(sc, BorderLayout.CENTER );
+			
+			
+			setVisible(true);
+			
+			btHome.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					dispose();
+					
+				}
+			});
+		}
+		
+		public static void main(String[] args) {
+			SwingUtilities.invokeLater(new Runnable() {
+				
+				@Override
+				public void run() {
+					new Tienda (null);
+					
+				}
+			});
+		}
 	}
-	
-//	public static void main(String[] args) {
-//		SwingUtilities.invokeLater(new Runnable() {
-//			
-//			@Override
-//			public void run() {
-//				new Tienda(null);
-//				
-//			}
-//		});
-//	}
-}
