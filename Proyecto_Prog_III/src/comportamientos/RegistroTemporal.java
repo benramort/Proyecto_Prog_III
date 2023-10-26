3eaa78b9ea9de7ed7f24ba1076cd08317ffc3fc3
Beñat Ramirez <benat.ramirez@gmail.com>
2023-10-26 18:21:53 +0200

    Registros temporales

---
+++ Proyecto_Prog_III/src/comportamientos/RegistroTemporal.java
@@ -0,0 +1,46 @@
+package comportamientos;
+
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegistroTemporal {
+	
+	Carta[] cartas;
+	double bonificacion;
+	ZonedDateTime fechaHora;
+	
+	public RegistroTemporal(Carta carta1, Carta carta2, Carta carta3, int bonificacion, ZonedDateTime fechaHora) {
+		cartas = new Carta[] {carta1, carta2, carta2};
+		this.bonificacion = bonificacion;
+		this.fechaHora = fechaHora;
+	}
+	
+	private void recuperarCartas(int cod1, int cod2, int cod3) {
+		cartas = new Carta[3];
+		for (Carta c: MiBaseDeDatos.modeloCartas) {
+			if (c.getId() == cod1) {
+				cartas[0] = c;
+			} else if (c.getId() == cod2) {
+				cartas[1] = c;
+			}else if (c.getId() == cod3) {
+				cartas[2] = c;
+			}
+		}
+		System.out.println(cartas[0]);
+		System.out.println(cartas[1]);
+		System.out.println(cartas[2]);
+	}
+	
+	public long calculoTiempoMinutos(ZonedDateTime fechaMenor, ZonedDateTime fechaMayor) {
+		return fechaMenor.until(fechaMayor, ChronoUnit.MINUTES); //De momento en minutos. Segundos: Más precisión
+	}
+	
+	public static void main(String[] args) {
+		RegistroTemporal r1 = new RegistroTemporal(null, null, null, 0, null);
+		MiBaseDeDatos.cargarModeloCartas();
+		r1.recuperarCartas(2,1,4);
+	}
+
+}
